be88fba69fad180c8575b63e63f0a13a828502b1
Alex <urus.urusik1337@gmail.com>
2023-11-18 16:04:30 +0700

    Delete Mappers

--- TestCaseRestApi/Mappers/DTO_Model/IMapperDM.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace TestCaseRestApi.Mappers.DTO_Model
-{
-    public interface IMapperDM<TModel, TModelDTO>
-    {
-        TModel ToModel(TModelDTO modelDTO);
-        TModelDTO ToModelDTO(TModel model);
-    }
-}
--- TestCaseRestApi/Mappers/Object_Model/IMapperOM.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace TestCaseRestApi.Mappers.Object_Model
-{
-    public interface IMapperOM<TModel, TObject>
-    {
-        TModel ToModel(TObject obj);
-        TObject ToObject(TModel model);
-    }
-}
